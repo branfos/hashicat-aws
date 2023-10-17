2e0abc97ec4309352f1541dc4fcc934884be3060
branfos <109987685+branfos@users.noreply.github.com>
2023-10-17 09:35:05 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -10,8 +10,8 @@ cat << EOM > /var/www/html/index.html
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  Greetings and Salutations!
+  <center><h2>Greetings and Salutations!</h2></center>
+  
   <!-- END -->
 
   </div>
