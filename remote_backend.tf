09eb4ec0a1363764ca0029f63b11b6ac89a9628c
branfos <brandon.foster@hashicorp.com>
2025-04-07 13:56:57 -0500

    changes

--- remote_backend.tf
+++ remote_backend.tf
@@ -5,7 +5,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "brandon_foster"
+    organization = "brandonfoster"
     workspaces {
       name = "hashicat-aws"
     }
